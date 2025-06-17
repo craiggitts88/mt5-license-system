ecc00a4b29ad3dcaa011a29f6ae437031e3d236f
craiggitts88 <craiggittins@googlemail.com>
2025-06-17 15:06:35 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MT5LicenseSystem from './components/MT5LicenseSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <MT5LicenseSystem />
+    </div>
+  );
+}
+
+export default App;
